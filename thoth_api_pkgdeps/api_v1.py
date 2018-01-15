6b18d966cf0ce0222cd7b83cdb46dbdd17643330
Fridolin Pokorny <fridolin@redhat.com>
2018-01-15 15:13:07 +0100

    Implement /api/v1/extract-build-log endpoint

--- thoth_api_pkgdeps/api_v1.py
+++ thoth_api_pkgdeps/api_v1.py
@@ -2,12 +2,17 @@
 
 import logging
 
+from thoth_pkgdeps import extract_build_log
+
 
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.setLevel(logging.INFO)
 
 
-def hello():
-    _LOGGER.info("Hello!")
-    return {"message": "Hello world!"}, 200
+def api_extract_build_log(build_log_info):
+    build_log = build_log_info.get('buildlog')
+    if not build_log:
+        return {"error": "No data to be processed"}, 400
 
+    result = extract_build_log(build_log)
+    return result, 200
