640f38bdc765d08244ea50ae316c7cf36401fd42
Fridolin Pokorny <fridolin@redhat.com>
2018-01-18 12:38:08 +0100

    Introduce extract-image endpoint

--- thoth_api_pkgdeps/api_v1.py
+++ thoth_api_pkgdeps/api_v1.py
@@ -2,17 +2,26 @@
 
 import logging
 
-from thoth_pkgdeps import extract_build_log
+from thoth_pkgdeps import extract_buildlog
+from thoth_pkgdeps import extract_image
 
 
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.setLevel(logging.INFO)
 
 
-def api_extract_build_log(build_log_info):
+def api_extract_buildlog(build_log_info):
     build_log = build_log_info.get('buildlog')
     if not build_log:
         return {"error": "No data to be processed"}, 400
 
-    result = extract_build_log(build_log)
+    result = extract_buildlog(build_log)
     return result, 200
+
+
+def api_extract_image(image):
+    # TODO: we will need to do this asynchronously as we can easily encounter timeouts.
+    try:
+        return extract_image(image, timeout=None)
+    except Exception as exc:
+        return {'error': str(exc)}, 400
